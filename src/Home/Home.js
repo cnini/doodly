a9f0e114eff04b802cca2ae2fb07439a2a8ea4b5
SadioToure <sadio.toure0109@gmail.com>
2024-02-08 16:43:57 +0100

    add images

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,8 +1,7 @@
 import { Text, View } from "react-native"
-
 import { styles } from "./HomeStyleSheet"
 
-export const Home = ({ navigation }) => {
+export const Home = () => {
     return (
         <View style={styles.homeContainer}>
             <Text>Home.js</Text>
