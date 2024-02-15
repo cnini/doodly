d1400dd51c7d2136515e96252c8b73383b7ec4ff
cnini <cynthia-zoely-nirina.rabemanantsoa@estiam.com>
2024-02-15 14:07:33 +0100

    signin OK

--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -1,10 +1,32 @@
-import { Text, View } from "react-native"
+import { Text, View, TextInput, Button } from "react-native"
 import { styles } from "./LoginStyleSheet"
+import { useState } from "react"
+import { auth } from "../../firebase"
 
-export const Login = () => {
+export const Login = ({ navigation }) => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSignin = () => {
+        if (email !== "" && password !== "") {
+            auth.signInWithEmailAndPassword(email, password)
+            .then(() => {
+                console.log("Utilisateur connecté")
+                navigation.navigate('Home')
+            })
+            .catch((e) => console.log(e))
+        }
+    }
+
     return (
         <View style={styles.loginContainer}>
-            <Text>Login.js</Text>
+            <Text style={styles.inputLabel}>Adresse mail</Text>
+            <TextInput style={styles.inputBox} onChangeText={setEmail}/>
+
+            <Text style={styles.inputLabel}>Mot de passe</Text>
+            <TextInput style={styles.inputBox} secureTextEntry={true} onChangeText={setPassword}/>
+
+            <Button title="Se connecter" onPress={handleSignin}/>
         </View>
     )
 }
