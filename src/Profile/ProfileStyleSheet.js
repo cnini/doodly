fdf6a36bc26bab9183af745e4484d943e87f03f7
cnini <cynthia-zoely-nirina.rabemanantsoa@estiam.com>
2024-02-22 04:20:57 +0100

    update front profile

--- src/Profile/ProfileStyleSheet.js
+++ src/Profile/ProfileStyleSheet.js
@@ -6,5 +6,25 @@ export const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
+
+    backgroundImage: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+    },
+
+    profileButton: {
+        width: 200,
+        height: 50,
+        backgroundColor: '#000',
+        borderRadius: 50,
+        marginTop: 20,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    profileButtonText: {
+        color: '#FFF'
+    },
     
 })
